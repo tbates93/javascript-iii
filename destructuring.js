f369466de7154d28d0ab7399d559b7425f1415d4
Travis Bates <travbates93@gmail.com>
2017-09-05 22:43:37 -0600

    Finished with destructuring.js 9/5/17

--- destructuring.js
+++ destructuring.js
@@ -2,7 +2,7 @@
 // DESTRUCTURING
 // ========================
 
-// Use object destructuring to save the property values from the object carDetails into new variables. 
+// Use object destructuring to save the property values from the object carDetails into new variables.
 
 var carDetails = {
   color: 'red',
@@ -13,6 +13,7 @@ var carDetails = {
 
 // CODE HERE
 
+var  {color, make, model, year} = carDetails;
 
 // ========================
 
@@ -20,8 +21,7 @@ var carDetails = {
 // In the function below named greeting, it is receiving an object as a parameter. Use object destructuring to save the object properties to new variables. The property names are firstName, lastName, and title. Return the concatenated string.
 
 function greeting( obj ) {
-  // CODE HERE
-  
+var {title,firstName,lastName} = obj;
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
 }
 
@@ -31,16 +31,23 @@ function greeting( obj ) {
 
 // Write a function called totalPopulation that will take in an object. That object will have 4 properties named utah, california, texas and arizona. The property values will be numbers. Use object destructuring to save the property values to new variables. Sum up the values and return the total number.
 
-  // CODE HERE
-
+var totalPopulation = (obj) => {
+  var {utah,california,texas,arizona} = obj;
+  return utah + california + texas + arizona;
+}
 
 // ========================
 
 
-// Write a function called ingredients that will take in an object. This object will have 3 properties named carb, fat, and protein. The property values will be strings. Use object destructuring to save the property values to new variables. Push these new variables to an array and return the array. 
+// Write a function called ingredients that will take in an object. This object will have 3 properties named carb, fat, and protein. The property values will be strings. Use object destructuring to save the property values to new variables. Push these new variables to an array and return the array.
 
-  // CODE HERE
+var ingredients = (obj) => {
+  var {carb,fat,protein} = obj;
+  var ingArr = []
+  ingArr.push(carb,fat,protein);
 
+  return ingArr;
+}
 
 // ========================
 
@@ -53,12 +60,32 @@ function greeting( obj ) {
 
 // Write a function called largeNumbers that will take a destructured object as it's parameter. The object properties will be named first, second, and third and their values will be numbers. Find the smallest number of the three and return that number.
 
-  // CODE HERE
+var largeNumbers = (obj) => {
+  var {first,second,third} = obj;
+  var arr = [];
+  arr.push(first,second,third);
+  arr.sort((a,b) => a-b);
+  return arr[0]
 
+}
 
 // ========================
 
 
-// Write a function called numberGroups that will take a destructured object as it's parameter. The object properties will be named a, b, and c and their values will be arrays of numbers. Find the longest array and return that array. 
+// Write a function called numberGroups that will take a destructured object as it's parameter. The object properties will be named a, b, and c and their values will be arrays of numbers. Find the longest array and return that array.
 
-  // CODE HERE
+var numberGroups = (obj) => {
+  var {a,b,c} = obj;
+
+  // console.log(a)
+  // console.log(b)
+  // console.log(c)
+  //
+  // console.log(a.length)
+  // console.log(b.length)
+  // console.log(c.length)
+  //
+  if (a.length > b.length && a.length > c.length) return a;
+  else if (b.length > a.length && b.length > c.length) return b;
+  else if (c.length > a.length && c.length > b.length) return c;
+}
